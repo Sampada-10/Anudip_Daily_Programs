42c1dfa27520d3a9c42ed4cc033617c3276c7e1c
Sampada-10 <136876867+Sampada-10@users.noreply.github.com>
2023-12-02 12:29:17 +0530

    Update PyramidPattern.java

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -2,20 +2,14 @@ package Patterns;
 import java.util.*;
 public class PyramidPattern 
 {
-    public static void main(String[] args)
-    {
-        for(int i=0;i<=5;i++)
-        {
-            for(int space=5-i;space>1;space--)
-            {
-                System.out.println(" ");
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int space=5-i;space>1;space--){
+                System.out.print(" ");
             }
-            for(int j=0;j<i*2+1;j++) 
-            {
+            for(int j=0;j<i*2+1;j++ )
                 System.out.print("*");
-            }
-            System.out.print("\n");
-         }
-    }
-
+            System.out.println();
+        }
+    }
 }
